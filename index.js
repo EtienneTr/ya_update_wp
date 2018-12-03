ce8a646ea6fbdbfad6782f76751452140f84cb3c
Etienne TROUILLET <tienou42@gmail.com>
2018-12-03 17:48:14 +0100

    added eslint and valid cmd

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
- const fsp = require("fs").promises;
+const fsp = require('fs').promises;
 
 const testFs = async () => {
-    await fsp.appendFile('test.txt', "OK !");
-    console.log("Done.");
+    await fsp.appendFile('test.txt', 'OK !');
+    console.log('Done.');
 };
 
 testFs();
